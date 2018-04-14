b1ebd692c2510f6e2e7f82cd94de8abdaa7f692b
Nikho Sagala <nikhosagala@gmail.com>
2018-04-15 00:20:30 +0700

    Prepare deploy to Heroku

--- app/settings.py
+++ app/settings.py
@@ -123,6 +123,17 @@ AUTH_USER_MODEL = 'employee.Employee'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # setup when deployed to Heroku
 
 django_heroku.settings(locals())
